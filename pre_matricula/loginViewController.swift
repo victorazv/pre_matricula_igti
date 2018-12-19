88adb8cbad183e244d153f4038f461fb68c31e51
Victor Nunes <victornunes@MacBook-Air-de-Victor.local>
2018-12-18 22:43:28 -0300

    Redirecionamendo para abas

--- pre_matricula/loginViewController.swift
+++ pre_matricula/loginViewController.swift
@@ -11,6 +11,20 @@ import UIKit
 class LoginViewController: UIViewController {
     @IBOutlet weak var btn_iniciar: UIButton!
     
+    let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+    
+    @IBAction func btn_press(_ sender: UIButton) {
+
+        let appDelegate = UIApplication.shared.delegate! as! AppDelegate
+        let initialViewController = self.storyboard!.instantiateViewController(withIdentifier: "tabs")
+        appDelegate.window?.rootViewController = initialViewController
+        appDelegate.window?.makeKeyAndVisible()
+        
+        //let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        //let pm_nova =  storyboard.instantiateViewController(withIdentifier: "pm_nova")
+        //self.present(pm_nova, animated:true, completion:nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
